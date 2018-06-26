9d613f177bdeb4f661bb45a8945979412b7d7acb
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2018-06-26 08:19:48 +0800

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ home = os.path.expanduser(".")
 train_file = os.path.join(home, "data", "squad", "cmrc2018_train.json")
 dev_file = os.path.join(home, "data", "squad", "cmrc2018_dev.json")
 test_file = os.path.join(home, "data", "squad", "cmrc2018_trial.json")
-glove_word_file = os.path.join(home, "data", "glove", "sgns.target.word-ngram.1-2.dynwin5.thr10.neg5.dim300.iter5")
+glove_word_file = os.path.join(home, "data", "word2vec", "sgns.target.word-ngram.1-2.dynwin5.thr10.neg5.dim300.iter5")
 LTP_DATA_DIR = os.path.join(home,"data","ltp")
 
 target_dir = "data"
