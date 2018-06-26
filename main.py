2af578b8219d4dc8c9a45a14fbe2e8cc9fef8be2
MobtgZhang <33253392+MobTgZhang@users.noreply.github.com>
2018-06-26 08:44:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import numpy as np
 import ujson as json
 import re
 from collections import Counter
-import string
 from tqdm import tqdm
 import random
 import torch
@@ -67,7 +66,7 @@ def convert_tokens(eval_file, qa_id, pp1, pp2):
     for qid, p1, p2 in zip(qa_id, pp1, pp2):
         context = eval_file[str(qid)]["context"]
         spans = eval_file[str(qid)]["spans"]
-        uuid = eval_file[str(qid)]["uuid"]
+        uuid = eval_file[str(qid)]["context_id"]
         l = len(spans)
         if p1 >= l or p2 >= l:
             ans = ""
@@ -92,27 +91,7 @@ def evaluate(eval_file, answer_dict):
     f1 = 100.0 * f1 / total
     return {'exact_match': exact_match, 'f1': f1}
 
-
-def normalize_answer(s):
-    def remove_articles(text):
-        return re.sub(r'\b(a|an|the)\b', ' ', text)
-
-    def white_space_fix(text):
-        return ' '.join(text.split())
-
-    def remove_punc(text):
-        exclude = set(string.punctuation)
-        return ''.join(ch for ch in text if ch not in exclude)
-
-    def lower(text):
-        return text.lower()
-
-    return white_space_fix(remove_articles(remove_punc(lower(s))))
-
-
 def f1_score(prediction, ground_truth):
-    prediction_tokens = normalize_answer(prediction).split()
-    ground_truth_tokens = normalize_answer(ground_truth).split()
     common = Counter(prediction_tokens) & Counter(ground_truth_tokens)
     num_same = sum(common.values())
     if num_same == 0:
@@ -124,8 +103,7 @@ def f1_score(prediction, ground_truth):
 
 
 def exact_match_score(prediction, ground_truth):
-    return (normalize_answer(prediction) == normalize_answer(ground_truth))
-
+    return (prediction == ground_truth)
 
 def metric_max_over_ground_truths(metric_fn, prediction, ground_truths):
     scores_for_ground_truths = []
